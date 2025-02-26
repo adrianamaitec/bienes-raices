f344b3ea42ac1e98b2cc8d8827e7d13d29cb93d1
adrianamaitec <88515150+adrianamaitec@users.noreply.github.com>
2025-02-26 15:14:03 -0400

    icons y imagenes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
+import type { Metadata } from 'next';
 import { Providers } from '@/components/providers';
+import { Roboto } from "next/font/google";
 
+const font = Roboto({
+  weight: ["100", "300", "400", "500", "700", "900"],
+  subsets: ["latin"],
+});
+export const metadata: Metadata = {
+  title: "RealState - 3D",
+  description: "REALSTATe website with 3D models",
+};
 export default function RootLayout({
   children,
 }: {
@@ -7,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es" className='dark'>
-      <body>
+      <body className={font.className}>
         <Providers>
           {children}
         </Providers>
